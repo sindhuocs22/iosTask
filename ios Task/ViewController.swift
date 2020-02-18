c6dbb2976555f4658cfcd00133eb5638fdafef30
Sindhu Elango <sindhuocs22@gmail.com>
2020-02-18 15:39:07 +0530

    Initial Commit

---
+++ ios Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios Task
+//
+//  Created by INFOSYS on 18/02/20.
+//  Copyright © 2020 INFOSYS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
